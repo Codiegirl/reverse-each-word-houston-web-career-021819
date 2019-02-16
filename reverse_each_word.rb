b6eb5ca27b59aba62696e045084d6742dd095bfd
Ro Flo <iroflood@msn.com>
2019-02-16 20:09:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,2 @@
 def reverse_each_word(sentence)
-  original_array = sentence.split(" ")
-  return_array = []
-  original_array.each do|sentence|
-    return_array << sentence.reverse
-  end
-  return_array.join(" ")
-end
+  
